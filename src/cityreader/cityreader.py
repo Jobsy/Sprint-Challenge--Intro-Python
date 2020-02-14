ed225d2d0482e7caf7d2f05888a478dffbbd11c9
Oluwajoba <oluwajobabello@gmail.com>
2020-02-14 15:20:08 +0100

    ad out string format to function / cityreader.py

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -106,13 +106,19 @@ def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
     for c in cities:
         # print(within.append(c.lat))
         if (int(c.lat) in (range(min(lat1, lat2), max(lat1, lat2)))) and (int(c.lon) in range(min(lon1, lon2), max(lon1, lon2))):
-            print(within.append((c.lat, c.lon)))
+            # print(within.append((c.lat, c.lon)))
+            #  filtered_cities = f"{c.name}: ({float(c.lat)}, {float(c.lon)})"
+            filtered_cities = City(c.name, float(c.lat), float(c.lon))
+            within.append(filtered_cities)
             # print(c.lat, "??????????????")
     # for lat in range(user_input_1.split(',')[0], user_input_2.split(',')[0],):
 
     # for lon in range(user_input_1.split(',')[1], user_input_2.split(',')[1],)
-    print(within)
-    # return within
+    # print(within)
+    for w in within:
+        print(w)
+
+    return within
 
 
 cityreader_stretch(45, -100, 32, -120, cities)
